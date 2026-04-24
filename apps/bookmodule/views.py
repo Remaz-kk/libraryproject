562f109d6f58c7169f8bc7adf6bd307964593622
Remaz-kk <kald73113@gmail.com>
2026-04-24 18:47:32 +0300

    update

--- apps/bookmodule/views.py
+++ apps/bookmodule/views.py
@@ -5,10 +5,87 @@ from .models import Book
 from django.shortcuts import render
 from django.db.models import Q, Count, Sum, Avg, Max, Min
 from .models import Book, Address, Student
-
+from .forms import BookForm
 
 from django.db.models import Count
 from .models import Address
+from django.shortcuts import render, redirect, get_object_or_404
+
+from .forms import BookForm
+from django.shortcuts import render
+from django.db.models import Sum
+from .models import Book
+
+
+from django.shortcuts import render
+from django.db.models import Sum
+from .models import Book
+
+from django.shortcuts import render
+from django.db.models import Sum
+from .models import Publisher
+from django.db.models import Sum
+from .models import Publisher
+
+def lab9_task6(request):
+    publishers = Publisher.objects.annotate(
+        filtered_books_count=Count(
+            'book',
+            filter=Q(book__price__gt=50) & Q(book__quantity__lt=5) & Q(book__quantity__gte=1)
+        )
+    )
+    return render(request, 'bookmodule/lab9/task6.html', {'publishers': publishers})
+
+
+def lab9_task5(request):
+    publishers = Publisher.objects.annotate(
+        high_rated_count=Count('book', filter=Q(book__rating__gte=4))
+    )
+    return render(request, 'bookmodule/lab9/task5.html', {'publishers': publishers})
+
+def lab9_task4(request):
+    publishers = Publisher.objects.annotate(
+        avg_price=Avg('book__price'),
+        min_price=Min('book__price'),
+        max_price=Max('book__price')
+    )
+    return render(request, 'bookmodule/lab9/task4.html', {'publishers': publishers})
+
+
+def lab9_task3(request):
+    publishers = Publisher.objects.all()
+    result = []
+
+    for pub in publishers:
+        oldest_book = pub.book_set.order_by('pubdate').first()
+        result.append({
+            'publisher': pub,
+            'oldest_book': oldest_book
+        })
+
+    return render(request, 'bookmodule/lab9/task3.html', {'result': result})
+
+
+
+def lab9_task2(request):
+    publishers = Publisher.objects.annotate(
+        total_book_stock=Sum('book__quantity')
+    )
+    return render(request, 'bookmodule/lab9/task2.html', {'publishers': publishers})
+
+def lab9_task1(request):
+    total_quantity = Book.objects.aggregate(total=Sum('quantity'))['total'] or 0
+    books = Book.objects.all()
+
+    for book in books:
+        if total_quantity > 0:
+            book.percentage = (book.quantity / total_quantity) * 100
+        else:
+            book.percentage = 0
+
+    return render(request, 'bookmodule/lab9/task1.html', {'books': books})
+
+
 
 def lab8_task7(request):
     cities = Address.objects.annotate(num_students=Count('student'))
